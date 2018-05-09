3b894c461e3727739548aeadfa22ded01c7595c0
temyers <tim.myerscough@mechanicalrock.io>
2018-05-09 10:40:30 +0000

    update - add GitHub source

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 reset
 
-#echo "Create the initial CloudFormation Stack"
-#aws cloudformation create-stack --stack-name "temyersGithubInceptionPipeline" --template-body file://aws_seed.yml --parameters file://aws_seed-cli-parameters.json --capabilities "CAPABILITY_NAMED_IAM" 
-#echo "Waiting for the CloudFormation stack to finish being created."
-#aws cloudformation wait stack-create-complete --stack-name "temyersGithubInceptionPipeline"
+echo "Create the initial CloudFormation Stack"
+aws cloudformation create-stack --stack-name "temyersGithubInceptionPipeline" --template-body file://aws_seed.yml --parameters file://aws_seed-cli-parameters.json --capabilities "CAPABILITY_NAMED_IAM" 
+echo "Waiting for the CloudFormation stack to finish being created."
+aws cloudformation wait stack-create-complete --stack-name "temyersGithubInceptionPipeline"
 # Print out all the CloudFormation outputs.
-#aws cloudformation describe-stacks --stack-name "temyersGithubInceptionPipeline" --output table --query "Stacks[0].Outputs"
+aws cloudformation describe-stacks --stack-name "temyersGithubInceptionPipeline" --output table --query "Stacks[0].Outputs"
 
 export CODECOMMIT_REPO=`aws --profile default cloudformation describe-stacks --stack-name "temyersGithubInceptionPipeline" --output text --query "Stacks[0].Outputs[?OutputKey=='CodeCommitRepositoryCloneUrlHttp'].OutputValue"`
 
